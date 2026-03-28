56a0a48e7ceec4d07581bfde0fe593f70cc2a36f
Indraneel Mandal <Indraneelmandal0387@gmail.com>
2026-03-28 18:11:32 +0530

    Implement transaction usage tracking
    Added functions to manage used transactions in local storage.

--- js/storage.js
+++ js/storage.js
@@ -13,6 +13,18 @@ renderTransactions()
 }
 
 }
+function getUsedTxs(){
+return JSON.parse(localStorage.getItem("usedTxs") || "[]")
+}
+
+function markTxUsed(id){
+
+let used = getUsedTxs()
+
+used.push(id)
+
+localStorage.setItem("usedTxs", JSON.stringify(used))
+}
 
 function saveTransaction(tx,type){
 
